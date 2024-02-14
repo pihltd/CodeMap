c4889a6c08392a64009dd568aff8e73bb42bc53b
Todd Pihl <todd.pihl@gmail.com>
2024-02-14 12:55:52 -0500

    Branch update for dictionary approach to headers

--- Bento2MapCode.py
+++ Bento2MapCode.py
@@ -2,18 +2,21 @@ import argparse
 import yaml
 import re
 import pandas as pd
+import pprint
+import sys
+#This version will attempt to future proof changes to the column names by using a dictionary where the key is the Excel column ID
+#TODO:  Make sure nodes are referred to as Nodes and properties as Properties in the Element Type and Characteristic Type columns
 
 def readConfigs(yamlfile):
     with open(yamlfile) as f:
         configs = yaml.load(f, Loader=yaml.FullLoader)
     return configs
 
-def buildConfigConstants(configjson):
+def buildConfigConstants(configjson, codemapdict):
     constants = {}
     for key, value in configjson['modelinfo'].items():
-        constants[key] = value
+        constants[codemapdict[key]] = value
     return constants
-
 
 def parseMDF(mdffile,type):
     with open(mdffile) as f:
@@ -23,13 +26,10 @@ def parseMDF(mdffile,type):
         modeldict = modeldict['PropDefinitions']
         return modeldict
     elif type == "model":
-        if "Handle" in modeldict:
-            repo = modeldict['Handle']
-        else:
-            repo = "CTDC"
         modeldict = modeldict['Nodes']
-        return modeldict, repo
-   
+        return modeldict
+
+
 def cleanline(text):
     if isinstance(text, str):
         text = re.sub(r'\W+',' ',text)
@@ -45,68 +45,96 @@ def buildLine(linedict, fieldlist):
             line.append("\t")
     return line
 
-def lineSet(node, configs):
+
+def lineSet(node, configs, codemapdict):
     line = {}
-    line = buildConfigConstants(configs)
-    line['Element Long Name'] = node
-    line['Element Physical Name'] = node
+    line = buildConfigConstants(configs, codemapdict)
+    #Element Long Name
+    line[codemapdict['h']] = node
+    #Element Physical Name
+    line[codemapdict['i']] = node
     return line
 
-def main(args):
+def nodeSet(node, configs, codemapdict):
+    line = {}
+    line = buildConfigConstants(configs, codemapdict)
+    line[codemapdict['h']] = node
+    line[codemapdict['i']] = node
+    line[codemapdict['k']] = 'Node'
+    line[codemapdict['p']] = 'Node'
+    return line
 
-    codemapfields = ["DO NOT USE","Batch User","Batch Name","Seq ID","*Model Name",	"*Model ID","*Model Version","Element Long  Name","*Element Physical Name","Element Description",
-                     "Element Type","Characteristic Long Name","*Characteristic Physical Name","Characteristic Order","Characteristic Description","Characteristic Type","Characteristic Min Length",
-                    "Characteristic Max Length","Characteristic Data Type", "Characteristic UOM","Characteristic Mandatory?","Characteristic PK?","Characteristic Default","CDE ID",
-                    "CDE Version","Element Mapping Group","Characteristic FK?","FK Element Name","FK Element Characteristic Name","Comments"]
+
+def main(args):
+    codemapdict = {"a":"DO NOT USE","b":"Batch User","c":"Batch Name","d":"Seq ID","e":"*Model Name","f":"*Model ID","g":"*Model Version","h":"Element Long  Name","i":"*Element Physical Name","j":"Element Description",
+                     "k":"Element Type","l":"Characteristic Long Name","m":"*Characteristic Physical Name","n":"Characteristic Order","o":"Characteristic Description","p":"Characteristic Type","q":"Characteristic Min Length",
+                    "r":"Characteristic Max Length","s":"Characteristic Data Type", "t":"Characteristic UOM","u":"Characteristic Mandatory?","v":"Characteristic PK?","w":"Characteristic Default","x":"CDE ID",
+                    "y":"CDE Version","z":"Element Mapping Group","aa":"Characteristic FK?","ab":"FK Element Name","ac":"FK Element Characteristic Name","ad":"Comments"}
+
     
-    configs = readConfigs(args.configfile)
-    modeldict, repo = parseMDF(configs['scriptinfo']['modelfile'],"model")
-    propdict = parseMDF(configs['scriptinfo']['propsfile'], "props")
+    codemapfields = []
+    for key, value in codemapdict.items():
+        codemapfields.append(value)
     datalist = []
+
+    configs = readConfigs(args.configfile)
+    modeldict = parseMDF(configs['scriptinfo']['modelfile'],"model")
+    propdict = parseMDF(configs['scriptinfo']['propsfile'], "props")
 
     index = 1
     for node, properties in modeldict.items():
-        line = lineSet(node, configs)
+        #Node is the graph node and properties is a dictionary associated with the property
+        nodeline = nodeSet(node, configs, codemapdict)
+        datalist.append(nodeline)
+        line = lineSet(node, configs, codemapdict)
         #properties['Props'] can be null
         if properties['Props'] is not None:
             for propname in properties['Props']:
                 if propname in propdict:
-                    line['Seq ID'] = index
+                    #Seq ID
+                    line[codemapdict['d']] = index
                     index = index + 1
-                    line['Characteristic Long Name'] = propname
-                    line['Characteristic Physical Name'] = propname
+                    #Characteristic Long Name'
+                    line[codemapdict['l']] = propname
+                    #Characteristic Physical Name
+                    line[codemapdict['m']] = propname
                     if "Desc" in propdict[propname]:
-                        line['Characteristic Description'] = cleanline(propdict[propname]['Desc'])
+                        #Characteristic Description
+                        line[codemapdict['o']] = cleanline(propdict[propname]['Desc'])
                     if "Type" in propdict[propname]:
                         #Type can have a "pattern" dict as a value, so only clean up if a string
                         if isinstance(propdict[propname]['Type'],str):
-                            line['Characteristic Type'] = cleanline(propdict[propname]['Type'])
+                            #Characteristic Type
+                            line[codemapdict['p']] = cleanline(propdict[propname]['Type'])
                     if "Req" in propdict[propname]:
                         req = propdict[propname]['Req']
                         if isinstance(req, bool):
                             req = 'Yes'
                         if req == 'Preferred':
                             req = None
-                        line['Characteristic Mandatory?'] = req
+                            #Characteristic Mandatory
+                        line[codemapdict['u']] = req
                     if "Term" in propdict[propname]:
                         #This gets a little funky as Term is a list of dictionary
                         for entry in propdict[propname]['Term']:
                             if entry['Origin'] == "caDSR":
                                 cdeid = cleanline(entry['Code'])
                                 if cdeid not in ['code ID']:
-                                    line['CDE ID'] = cleanline(entry['Code'])
+                                    #CDE ID
+                                    line[codemapdict['x']] = cleanline(entry['Code'])
                                 else:
-                                    line['Element Mapping Group (Optional if no CDE is associated)'] = node
+                                    #Element Mapping Group (Optional if no CDE is associated)
+                                    line[codemapdict['z']] = node
                             else:
-                                line['Element Mapping Group (Optional if no CDE is associated)'] = node
+                                line[codemapdict['z']] = node
                     else:
-                        line['Element Mapping Group (Optional if no CDE is associated)'] = node
+                        line[codemapdict['z']] = node
                     #At this point we've goten everything from the MDF file
                     datalist.append(line)
                     #Reset the line
-                    line = lineSet(node, configs)
+                    line = lineSet(node, configs, codemapdict)
                 else:
-                    line = lineSet(node, configs)
+                    line = lineSet(node, configs, codemapdict)
     #Create the dataframe
     finaldf = pd.DataFrame(datalist, columns=codemapfields)
     finaldf.to_csv(configs['scriptinfo']['outputfile'], sep="\t", index=False)
